2dc6a2634f14d007ea0b6c057a81ea22765ff973
Nicolas Geniteau <nicolas@cri.epita.net>
2017-03-23 20:49:43 +0100

    add BETA setting to use accounts-beta
    use accounts-beta.cri.epita.net instead of accounts.cri.epita.net if
    SOCIAL_AUTH_EPITA_BETA is True.
    
    Signed-off-by: Nicolas Geniteau <nicolas@cri.epita.net>

--- epita_connect/backend.py
+++ epita_connect/backend.py
@@ -6,5 +6,12 @@ from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 class EpitaOpenIdConnect(OpenIdConnectAuth):
     name = 'epita'
-    OIDC_ENDPOINT = 'https://accounts-beta.cri.epita.net/oidc'
+    OIDC_ENDPOINT = 'https://accounts.cri.epita.net/oidc'
 
+    def oidc_config(self):
+        if self.setting('BETA', default=False):
+            endpoint = 'https://accounts-beta.cri.epita.net/oidc'
+        else:
+            endpoint = self.OIDC_ENDPOINT
+        return self.get_json(endpoint +
+                             '/.well-known/openid-configuration')
