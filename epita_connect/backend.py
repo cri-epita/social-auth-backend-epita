daa2ebae2e6da6205ce50e186c91ae93f1d15459
Nicolas Geniteau <nicolas@cri.epita.net>
2017-08-18 16:29:15 +0200

    change oidc_endpoint
    accounts is moving to intra.cri.epita.net
    
    Signed-off-by: Nicolas Geniteau <nicolas@cri.epita.net>

--- epita_connect/backend.py
+++ epita_connect/backend.py
@@ -6,7 +6,7 @@ from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 class EpitaOpenIdConnect(OpenIdConnectAuth):
     name = 'epita'
-    OIDC_ENDPOINT = 'https://accounts.cri.epita.net/oidc'
+    OIDC_ENDPOINT = 'https://intra.cri.epita.net/oidc'
 
     def oidc_config(self):
         if self.setting('BETA', default=False):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='social-auth-backend-epita',
-    version='0.0.3',
+    version='0.0.4',
     author='Nicolas Geniteau',
     author_email='nicolas@lse.epita.fr',
     description='Social Auth backend for epita connect',
